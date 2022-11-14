5651560e9c91f86dba72c01405358650981e468c
Brendan <bwoods51501@gmail.com>
2022-11-14 13:40:24 -0500

    Initial commit

---
+++ src/ReachabilityMatrix.java
@@ -0,0 +1,19 @@
+// Name: Brendan Woods
+// Class: CS 3305/01
+// Term: Fall 2022
+// Instructor: Dr. Haddad
+// Assignment: 9
+// IDE Name: IntelliJ
+
+import java.util.Scanner;
+
+public class ReachabilityMatrix {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void printMenu() {
+
+    }
+}
